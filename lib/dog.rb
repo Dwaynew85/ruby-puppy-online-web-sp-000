afe3270b7e56e89528728dee1154612605ca43a2
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-05-15 22:04:20 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,29 @@
+class Dog 
+  attr_accessor :name 
+
+  @all = []
+
+  def initialize(name)
+    @name = name 
+    save 
+  end 
+
+  def save 
+    @@all << self 
+  end 
+
+  def self.all 
+    @@all 
+  end 
+
+  def self.clear_all 
+    self.all.clear 
+  end 
+
+  def self.print_all 
+    self.all.each do |s|
+      puts s.name 
+    end 
+  end
+
+end
